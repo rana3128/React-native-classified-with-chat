ad4160798811cdefaf78c1dfb5709b500080e608
abhionstak <117984203+abhionstak@users.noreply.github.com>
2023-05-10 15:07:41 +0530

    Fixes

--- api-findtoesay-service/start.ps1
+++
@@ -1,6 +0,0 @@
-env:mongoUri="mongodb://localhost:27017/findtoeasy"
-env:secret="rana@9808403128"
-env:JWT_SECRET="rana@9808403128"
-env:awsAccessKeyId="AKIAQLDAYMS7CX2RHPPN"
-env:awsSecretAccessKey="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"
-node src/server.js
